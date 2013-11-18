db5a9eadfa1ef3ef15a6b083ad3566c33890ad77
whitecrow0723@gmail.com <whitecrow0723@gmail.com@deb8c2bd-5405-09d6-a8b9-757064015c65>
2013-11-18 05:56:56 +0000

    http://code.google.com/p/number-baseball-game/issues/detail?id=7 - 사용자에게 숫자를 가져오는 기능을 추가합니다.

--- trunk/NumberBaseball/src/NumberBaseball.java
+++ trunk/NumberBaseball/src/NumberBaseball.java
@@ -1,49 +1,65 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 import org.apache.log4j.Logger;
 
-
 public class NumberBaseball {
-	
+
 	private static final Logger logger = Logger.getLogger(NumberBaseball.class);
-	
+
 	public static void main(String[] args) {
+		int[] a = getNumbersFromUser();
+		System.out.println(a[0] + "-" + a[1] + "-" + a[2]);
 		// TODO Auto-generated method stub
-		
+
 		logger.debug("숫자 야구 게임 시작!");
-		
-		
-		int[] showNum = new int[3]; //컴퓨터에서 뽑은 정수 3개 
-		int[] inputNum = new int[3]; //플레이어가 뽑은 정수 3개 
-		
-		//정수 3 만들기 
+
+		int[] showNum = new int[3]; // 컴퓨터에서 뽑은 정수 3개
+		int[] inputNum = new int[3]; // 플레이어가 뽑은 정수 3개
+
+		// 정수 3 만들기
 		Random rNum = new Random();
-		
+
 		showNum[0] = rNum.nextInt(10);
 		showNum[1] = rNum.nextInt(10);
 		showNum[2] = rNum.nextInt(10);
-	
-		if(showNum[0] == showNum[1]){
+
+		if (showNum[0] == showNum[1]) {
 			showNum[1] = rNum.nextInt(10);
 		}
-		
-		if(showNum[0] == showNum[2]){
+
+		if (showNum[0] == showNum[2]) {
 			showNum[2] = rNum.nextInt(10);
-		}else if(showNum[1] == showNum[2]){
+		} else if (showNum[1] == showNum[2]) {
 			showNum[2] = rNum.nextInt(10);
 		}
 
 		logger.debug("숫자 3개를 선택하세요!");
-		
-		for(int i=0;i<inputNum.length;i++){
+
+		for (int i = 0; i < inputNum.length; i++) {
 			inputNum[i] = rNum.nextInt(10);
 		}
-		
-		//중복체크 해야함!!  
-		
-		
-		logger.debug("입력한 수 : " +Arrays.toString(inputNum));
+
+		// 중복체크 해야함!!
+
+		logger.debug("입력한 수 : " + Arrays.toString(inputNum));
+	}
+
+	private static int[] getNumbersFromUser() {
+		final int[] result = new int[3];
+		final Scanner a = new Scanner(System.in);
+		int aNumber = 0;
+		do {
+			System.out.println("숫자를 입력하세요.");
+			aNumber = a.nextInt();
+		} while (aNumber < 99 || aNumber > 999);
+
+		for (int i = result.length - 1; i >= 0; i--) {
+			result[i] = aNumber % 10;
+			aNumber = aNumber / 10;
+		}
+		return result;
 	}
 
 }
